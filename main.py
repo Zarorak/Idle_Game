e97580e702d69cf7cac48def623ce2eed742495d
Zarorak <martijnbh@hotmail.com>
2024-10-23 00:37:53 +0200

    Made Timer object

--- main.py
+++ main.py
@@ -1,67 +1,80 @@
 import time
-from datetime import datetime
 import cv2
 import numpy as np
-
-
-windowName = "main"
-lastWindowUpDate = time.time()
-updateWindowEveryMs = 500
+from threading import Thread, Event
 
 def main():
-
-    incPerSec = 1.1
-    multiplierPerSec = 1.0002
-    value = 0
     previousTime = time.time()
     startTime = previousTime
 
+    showWindowTimer = Timer(5)
+    showWindowTimer.start()
+    terminateTimer = Timer(5000)
+    terminateTimer.start()
+
     while(True):
         now = time.time()
-        deltaTime = now - previousTime
+    
         othervalue = now - startTime
-        
-        value += deltaTime 
+        if (showWindowTimer.isFlagged()):
+            UpdateWindow(othervalue)
 
+        if(terminateTimer.isFlagged()):
+            print("Stopping program")
+            showWindowTimer.stop()
+            terminateTimer.stop()
 
-        if ((now - lastWindowUpDate)*1000 >= updateWindowEveryMs):
-            UpdateWindow(value, othervalue)
-
-def UpdateWindow(newValue, otherval):
+def UpdateWindow(otherval):
 
     height, width = 400 ,1000
 
     window = np.zeros([height,width,3])
    
-
-    # font
     font = cv2.FONT_HERSHEY_SIMPLEX
-
-    # org
-    org = (200, 100)
-    org2 = (200,200)
-    # fontScale
+    org = (200,200)
     fontScale = 1
-    
-    # Blue color in BGR
     color = (255, 0, 0)
-
-    # Line thickness of 2 px
     thickness = 2
-    
-    # Using cv2.putText() method
-    window = cv2.putText(window, str(newValue/1000) , org, font, 
+    window = cv2.putText(window, str(otherval) , org, font, 
                     fontScale, color, thickness, cv2.LINE_AA)
-    window = cv2.putText(window, str(otherval) , org2, font, 
-                    fontScale, color, thickness, cv2.LINE_AA)
+    cv2.imshow("main", window)
+    cv2.waitKey(1)
     
 
-    cv2.imshow(windowName, window)
-    cv2.waitKey(2)
-    lastWindowUpDate = time.time()
-    return
+class Timer:
 
+    def __init__(self, intervalMs) -> None:
+        self.IntervalMs = intervalMs/1000
+        self.LastEvent = time.time()
+        self.Event = Event()
+        self.Thread = None
+        self.Started = False
 
+    def start(self):
+        self.Started = True
+        self.Thread = Thread(target=self.clock)
+        self.Thread.start()
+
+    def isFlagged(self)->bool :
+        if (self.Event.is_set()):
+            self.Event.clear()
+            return True
+        return False
+
+    def stop(self):
+        self.Started = False
+
+    def clock(self):
+        print("Clock started")
+        
+        while (self.Started):
+            now = time.time()
+            
+            if (now - self.LastEvent > self.IntervalMs):
+                self.Event.set()
+                self.LastEvent = now
+        print("Clock stopped")
+        return
 
 if __name__ == "__main__":
     main()
